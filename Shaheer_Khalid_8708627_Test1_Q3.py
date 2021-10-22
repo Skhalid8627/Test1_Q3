2bdb266527fd0223d43b0411f5e71d9547f2a083
Shaheer Khalid <skhalid8627@conestogac.on.ca>
2021-10-22 14:36:23 +0000

    Did not study classes and methods - my apologies.

---
+++ Shaheer_Khalid_8708627_Test1_Q3.py
@@ -0,0 +1,15 @@
+class Temperatures:
+    def __init__(self, temp ):
+        self.temp = temp
+
+    def enterTemp(self):
+        for i in range(3):
+            m.n = int(input("Enter the temperature %d : "%(self.temp)))
+
+
+    def displayTemp(self):
+        print(self.temp, )
+        print(self.)
+    def calcAverageTemp(self):
+        summation = sum(self.temp)
+        d.b = summation 
